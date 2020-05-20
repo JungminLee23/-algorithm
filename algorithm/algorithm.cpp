e207211aa6a74ede29b30cdba186388cf6a07f47
JungminLee <zkrns23@gmail.com>
2020-05-20 10:48:04 +0900

    소수 판별법 여러가지

--- algorithm/algorithm.cpp
+++ algorithm/algorithm.cpp
@@ -119,6 +119,7 @@ void main() {
 */
 
 // 5. 1~100까지 가장 큰 소수(번외로 입력하는 것 까지)
+/*
 void main() {
 	int n = 3, p = 2, num;
 	cout << "어디까지 큰 수를 구할까? ";
@@ -136,26 +137,98 @@ void main() {
 		}
 		if (pan == 0) {
 			p = n;
+		}+
+	}
+	cout << "가장 큰 소수는 " << p << " 입니다. " << endl;
+}
+*/
+// 5-1 new 방법
+void main() {
+	int n = 3, p = 2, num;
+	cout << "어디까지 큰 수를 구할까? ";
+	cin >> num;
+	// n -> 100까지 가는거 / 판별할 숫자
+	// p -> 가장 큰 소수를 담는 변수?
+
+	for (; n < num; n++) {
+		int sq = sqrt(n); // sqrt(n) --> n의 제곱근을 구하는 것.
+		for (int i = 2; i <= sq; i++) {
+			if (n%i == 0) {
+				break;
+			}
+			else if (i == sq) {
+				p = n;
+			}
 		}
 	}
 	cout << "가장 큰 소수는 " << p << " 입니다. " << endl;
 }
 
-// 5-1. 쌤 방법
+
+// 5-2. 쌤 방법
 /*
 void main() {
-	int n, flag = 0, i, sq;
-	cout << "숫자를 입력하세요 : ";
-	cin >> n;
-	sq = sqrt(n);
-	cout << "분석 숫자 : " << sq;
-	for (i = 2; i < sq; i++) {
-		if ((sq%i) == 0) {
-			flag = 1;
-			break;
+	int P = 2;
+	int N = 3;
+	while (1) {
+		int M = int(sqrt(double(N))); // 자바 math.sqrt(double(N))
+		//N(49)를 소수인지 판단하는 범위 (i(2) ~ M(7))
+		for (int i = 2; i <= M; i++) {
+			int R = N % i;
+			if (R == 0) break;
+			if (i == M) P = N; // 나눠 떨어진 수가 자기 자신인지 판단.
+									// 소수면 P변수 방에 저장
+		}
+		N++;
+		if (N > 100) break;
+	}
+	printf("100까지의 소수 중 가장 큰 수는 : %d\n", P);
+}
+*/
+
+
+// 5-3. 출력까지 한 소수 판별법
+/*
+void main() {
+
+	int i, j, k, m, cnt = 0, max = 0;
+
+	cout << "수를 입력 : ";
+	cin >> k;
+	m = sqrt(k);
+
+	//소수 판별
+	for (i = 2; i <= m; i++) {
+		if (k%i == 0) {
+			cnt++;
 		}
 	}
-	if (flag == 0) cout << "은(는) 소수 입니다." << endl;
-	else cout << "은(는) 소수가 아닙니다." << endl;
+	if (cnt == 1) {
+		cout << k << " 은(는) 소수가 맞습니다." << endl;
+	}
+	else
+	{
+		cout << k << " 은(는) 소수가 아닙니다." << endl;
+	}
+
+	cnt = 0; //판별 - 탐색을 이어지게 하기위해서 줌
+
+	//소수 탐색
+	cout << "0부터 " << k << "까지의 수중 소수는 ";
+
+	for (i = 2; i <= k; i++) {
+		for (j = 1; j <= i; j++) {
+			if (i%j == 0) {
+				cnt++;
+			}
+		}
+		if (cnt <= 2) {
+			cout << i << " ";
+			max = i;
+		}
+		cnt = 0;
+	}
+	cout << "입니다." << endl;
+	cout << "그중 가장 큰 소수는 " << max << " 입니다." << endl;
 }
 */
